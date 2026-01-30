fdda374b10c6134f9e50bb0d96904f6369b49c0a
Darin Kelkhoff <darin.kelkhoff@gmail.com>
2026-01-30 09:27:18 -0600

    test: update to more robust version of WebDriverManager.chromedriver call (try to avoid out-of-sync version errors)

--- src/test/java/com/kingsrook/qqq/frontend/materialdashboard/selenium/lib/QBaseSeleniumTest.java
+++ src/test/java/com/kingsrook/qqq/frontend/materialdashboard/selenium/lib/QBaseSeleniumTest.java
@@ -72,7 +72,10 @@ public class QBaseSeleniumTest
          chromeOptions.addArguments("--headless=new");
       }
 
-      WebDriverManager.chromedriver().setup();
+      WebDriverManager.chromedriver()
+         .clearDriverCache()
+         .clearResolutionCache()
+         .setup();
    }
 
 
